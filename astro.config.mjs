5f3b21d04b72adb81a39e84fae382ff369c4b918
VedantAndhale <90826179+VedantAndhale@users.noreply.github.com>
2024-06-15 21:34:09 +0530

    u#3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
-import critters from 'astro-critters'
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Vedant.me",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false }),critters()],
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
