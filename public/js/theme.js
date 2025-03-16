bb9b7e00adbaf9c41052fc3170181ef8e7716448
VedantAndhale <90826179+VedantAndhale@users.noreply.github.com>
2025-03-16 18:15:54 +0530

    v5.9

--- public/js/theme.js
+++ public/js/theme.js
@@ -2,30 +2,16 @@ function changeTheme() {
   const element = document.documentElement;
   const theme = element.classList.contains("dark") ? "light" : "dark";
 
-  const css = document.createElement("style");
-
-  css.appendChild(
-    document.createTextNode(
-      `* {
-           -webkit-transition: none !important;
-           -moz-transition: none !important;
-           -o-transition: none !important;
-           -ms-transition: none !important;
-           transition: none !important;
-        }`,
-    ),
-  );
-  document.head.appendChild(css);
-
   if (theme === "dark") {
     element.classList.add("dark");
   } else {
     element.classList.remove("dark");
   }
 
-  window.getComputedStyle(css).opacity;
-  document.head.removeChild(css);
   localStorage.theme = theme;
+
+  // Reload the page after theme change
+  window.location.reload();
 }
 
 function preloadTheme() {
